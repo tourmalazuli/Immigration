1c9d0f0ee7fae40e56a0c8f8a0cb28472dd71c4c
tourmalazuli <tourmalazuli@gmail.com>
2015-07-24 01:32:57 +0900

    First Commit

---
+++ Immigration/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Immigration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナ変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナで生成されたコード
+
+        /// <summary>
+        /// デザイナ サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディタで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.buttonまずここ = new System.Windows.Forms.Button();
+            this.button貼り付け = new System.Windows.Forms.Button();
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.textBoxメッセージ = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button初期ページ = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(12, 202);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(768, 352);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("https://www.immi-moj.go.jp/immimail/datainput.php", System.UriKind.Absolute);
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            this.webBrowser1.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser1_Navigated);
+            // 
+            // buttonまずここ
+            // 
+            this.buttonまずここ.Location = new System.Drawing.Point(133, 32);
+            this.buttonまずここ.Name = "buttonまずここ";
+            this.buttonまずここ.Size = new System.Drawing.Size(93, 23);
+            this.buttonまずここ.TabIndex = 1;
+            this.buttonまずここ.Text = "まずここを押す";
+            this.buttonまずここ.UseVisualStyleBackColor = true;
+            this.buttonまずここ.Click += new System.EventHandler(this.buttonまずここ_Click);
+            // 
+            // button貼り付け
+            // 
+            this.button貼り付け.Location = new System.Drawing.Point(232, 32);
+            this.button貼り付け.Name = "button貼り付け";
+            this.button貼り付け.Size = new System.Drawing.Size(75, 23);
+            this.button貼り付け.TabIndex = 4;
+            this.button貼り付け.Text = "貼り付け";
+            this.button貼り付け.UseVisualStyleBackColor = true;
+            this.button貼り付け.Click += new System.EventHandler(this.button貼り付け_Click);
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Location = new System.Drawing.Point(12, 8);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.ReadOnly = true;
+            this.textBoxURL.Size = new System.Drawing.Size(768, 19);
+            this.textBoxURL.TabIndex = 5;
+            // 
+            // textBoxメッセージ
+            // 
+            this.textBoxメッセージ.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxメッセージ.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBoxメッセージ.Location = new System.Drawing.Point(12, 277);
+            this.textBoxメッセージ.Name = "textBoxメッセージ";
+            this.textBoxメッセージ.Size = new System.Drawing.Size(768, 24);
+            this.textBoxメッセージ.TabIndex = 6;
+            this.textBoxメッセージ.Text = "読み込み中";
+            this.textBoxメッセージ.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(313, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(58, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "更新";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button初期ページ
+            // 
+            this.button初期ページ.Location = new System.Drawing.Point(12, 32);
+            this.button初期ページ.Name = "button初期ページ";
+            this.button初期ページ.Size = new System.Drawing.Size(115, 23);
+            this.button初期ページ.TabIndex = 8;
+            this.button初期ページ.Text = "初期ページへ移動";
+            this.button初期ページ.UseVisualStyleBackColor = true;
+            this.button初期ページ.Click += new System.EventHandler(this.button初期ページ_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 62);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(768, 134);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 566);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button初期ページ);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxメッセージ);
+            this.Controls.Add(this.textBoxURL);
+            this.Controls.Add(this.button貼り付け);
+            this.Controls.Add(this.buttonまずここ);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "入管通報";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button buttonまずここ;
+        private System.Windows.Forms.Button button貼り付け;
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.TextBox textBoxメッセージ;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button初期ページ;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Immigration/Form1.cs
@@ -0,0 +1,575 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Text.RegularExpressions;
+//using System.Data.OleDb;
+using System.IO;
+
+namespace Immigration
+{
+    public partial class Form1 : Form
+    {
+        string configFilePath = System.IO.Path.Combine(Application.StartupPath, "List.csv");
+        string kenFilePath = System.IO.Path.Combine(Application.StartupPath, "KEN.csv");
+
+        private DataTable _connectionList = new DataTable();
+        private DataTable _connectionKEN = new DataTable();
+
+        string _url = string.Empty;
+
+        string _uname = string.Empty;
+        string _usex = string.Empty;
+        string _uage = string.Empty;
+        string _ucontry = string.Empty;
+        string _uaddress = string.Empty;
+        string[] _uphone = new string[3];
+        string _email = string.Empty;
+
+        string _nationality = string.Empty;
+        string _details = string.Empty;
+
+        string _nickname = string.Empty;    //名称
+        string _workname = string.Empty;    //働いている場所の名称
+        string _workcity = string.Empty;    //働いている場所の住所（県名以降）
+
+        DataGridViewRow drow = new DataGridViewRow();
+        DataView dataSource;
+
+        //DataSet dataset = new DataSet();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //string configFilePath = System.IO.Path.Combine(Application.StartupPath, "List.csv");
+            StreamReader sr = new StreamReader(configFilePath, System.Text.Encoding.GetEncoding(932));
+
+            this._connectionList.Columns.Add("ID");
+            this._connectionList.Columns.Add("NAME");
+            this._connectionList.Columns.Add("GROUP");
+            this._connectionList.Columns.Add("ADDRESS");
+            this._connectionList.Columns.Add("CommitID");
+            this._connectionList.Columns.Add("DONE");
+
+            while (sr.Peek() >= 0)
+            {
+                string line = sr.ReadLine();
+                string[] vals = line.Split(',');
+
+                DataRow row = this._connectionList.NewRow();
+                row["ID"] = vals[0];
+                row["NAME"] = vals[1];
+                row["GROUP"] = vals[2];
+                row["ADDRESS"] = vals[3];
+                row["CommitID"] = vals[4];
+                row["DONE"] = vals[5];
+                this._connectionList.Rows.Add(row);
+            }
+
+            this._connectionList.PrimaryKey = new DataColumn[] { this._connectionList.Columns["ID"] };
+            dataSource = new DataView(this._connectionList);
+
+            this.dataGridView1.DataSource = dataSource;
+            this.dataGridView1.MultiSelect = false;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            loadSetting();
+
+            //郵便番号CSV格納
+            StreamReader srken = new StreamReader(kenFilePath, System.Text.Encoding.GetEncoding(932));
+            this._connectionKEN.Columns.Add("num");
+            this._connectionKEN.Columns.Add("ken");
+
+            while (srken.Peek() >= 0)
+            {
+                string lineKen = srken.ReadLine();
+                string[] valsken = lineKen.Split(',');
+
+                DataRow rowken = this._connectionKEN.NewRow();
+                rowken["num"] = valsken[0];
+                rowken["ken"] = valsken[1];
+                this._connectionKEN.Rows.Add(rowken);
+            }
+
+
+
+
+            //OleDbConnection connection = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0; Data Source=" + Application.StartupPath + "\\; Extended Properties=\"Text;HDR=YES;FMT=Delimited\"");
+
+            //OleDbCommand command = new OleDbCommand("SELECT * FROM [KEN.CSV]", connection);
+
+            //dataset.Clear();    // 念のためクリア
+
+            //OleDbDataAdapter adapter = new OleDbDataAdapter(command);
+            //adapter.Fill(dataset);
+
+            sr.Close();
+            srken.Close();
+
+        }
+
+        private void webBrowser1_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            this.Navigated();
+        }
+
+
+        private void button貼り付け_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                drow = getselectedRow(dataGridView1);
+
+                if (this.dataGridView1.CurrentRow.Cells[5].Value.ToString() != "済")
+                {
+
+                    if (drow != null)
+                    {
+                        _nickname = drow.Cells[1].Value.ToString();
+                        _workname = drow.Cells[2].Value.ToString();
+                        _workcity = drow.Cells[3].Value.ToString();
+                    }
+
+
+
+
+                    HtmlDocument doc1 = this.webBrowser1.Document;
+
+
+                    foreach (HtmlElement textArea in doc1.GetElementsByTagName("textarea"))
+                    {
+                        if (textArea.Name.Equals("details"))
+                        {
+                            textArea.SetAttribute("value", _details);
+                        }
+                    }
+
+                    foreach (HtmlElement input in doc1.GetElementsByTagName("input"))
+                    {
+                        if (input.Name.Equals("usrname"))
+                        {
+                            input.SetAttribute("value", _uname);
+                        }
+                        if (input.Name.Equals("age"))
+                        {
+                            input.SetAttribute("value", _uage);
+                        }
+                        if (input.Name.Equals("usrcity"))
+                        {
+                            input.SetAttribute("value", _uaddress);
+                        }
+
+                        if (_uphone[0] != null)
+                        {
+                            if (input.Name.Equals("usrtelno1"))
+                            {
+                                input.SetAttribute("value", _uphone[0]);
+                            }
+                            if (input.Name.Equals("usrtelno2"))
+                            {
+                                input.SetAttribute("value", _uphone[1]);
+                            }
+                            if (input.Name.Equals("usrtelno3"))
+                            {
+                                input.SetAttribute("value", _uphone[2]);
+                            }
+                        }
+
+
+                        if (input.Name.Equals("mail"))
+                        {
+                            input.SetAttribute("value", _email);
+                        }
+                        if (input.Name.Equals("nationality"))
+                        {
+                            input.SetAttribute("value", _nationality);
+                        }
+                        if (input.Name.Equals("nickname"))
+                        {
+                            input.SetAttribute("value", _nickname);
+                        }
+                        if (input.Name.Equals("workname"))
+                        {
+                            input.SetAttribute("value", _workname);
+                        }
+                        if (input.Name.Equals("workcity"))
+                        {
+                            input.SetAttribute("value", _workcity);
+                        }
+                    }
+
+                    HtmlElementCollection selectCollection = doc1.GetElementsByTagName("select");
+                    HtmlElement select = selectCollection["mencnt"];
+                    HtmlElementCollection optionCollection = select.GetElementsByTagName("option");
+                    HtmlElement option1 = optionCollection[16];
+                    option1.SetAttribute("selected", "true");
+
+                    select = selectCollection["womencnt"];
+                    optionCollection = select.GetElementsByTagName("option");
+                    option1 = optionCollection[16];
+                    option1.SetAttribute("selected", "true");
+
+                    select = selectCollection["bustype"];
+                    optionCollection = select.GetElementsByTagName("option");
+                    option1 = optionCollection[15];
+                    option1.SetAttribute("selected", "true");
+
+                    setSex();
+                    setken();
+
+                    if (yuubin(_workcity) != null)
+                    {
+                        setWorkKen(kenmei(yuubin(_workcity)));
+                    }
+                    else if (kenmeiGet(_workcity) != null)
+                    {
+                        setWorkKen(kenmeiGet(_workcity));
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("すでに送信済みです。\n", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception e1)
+            {
+                MessageBox.Show("エラーが発生しました。\n" + e1, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private HtmlDocument setLoadAttri()
+        {
+            HtmlDocument doc = this.webBrowser1.Document;
+
+            HtmlElementCollection selects = doc.GetElementsByTagName("select");
+            HtmlElement select = selects["info"];
+
+            if (select != null)
+            {
+                foreach (HtmlElement child in select.Children)
+                {
+                    if (child.InnerText == "働いている場所の情報のみ")
+                    {
+                        child.SetAttribute("selected", "true");
+                    }
+                }
+            }
+
+            select.RaiseEvent("onchange");
+            return doc;
+        }
+
+        private void Navigated()
+        {
+            this.textBoxURL.Text = this.webBrowser1.Url.ToString();
+
+            this.textBoxメッセージ.Visible = false;
+
+            if (this.webBrowser1.Url.ToString().Equals(_url))
+            {
+                this.button貼り付け.Enabled = true;
+            }
+            else
+            {
+                this.button貼り付け.Enabled = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.webBrowser1.Refresh();
+        }
+
+        private void button初期ページ_Click(object sender, EventArgs e)
+        {
+            this.webBrowser1.Url = new Uri(_url);
+        }
+
+
+
+        public DataGridViewRow getselectedRow(DataGridView dataGridView2)
+        {
+            if (dataGridView2.SelectedRows.Count > 0)
+            {
+                DataGridViewRow sourceRow = dataGridView2.SelectedRows[0];
+
+                DataGridViewRow destinationRow = new DataGridViewRow();
+
+                destinationRow.CreateCells(dataGridView2);
+
+                for (int i = 0; i < sourceRow.Cells.Count; i++)
+                {
+                    destinationRow.Cells[i].Value = sourceRow.Cells[i].Value;
+                }
+
+
+                return destinationRow;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+
+        }
+
+        public void loadSetting()
+        {
+            _url = ConfigurationManager.AppSettings["url"];
+            _uname = ConfigurationManager.AppSettings["name"];
+            _usex = ConfigurationManager.AppSettings["sex"];
+            _uage = ConfigurationManager.AppSettings["age"];
+            _ucontry = ConfigurationManager.AppSettings["contry"];
+            _uaddress = ConfigurationManager.AppSettings["address"];
+            //_uphone = ConfigurationManager.AppSettings["phone"];
+            _email = ConfigurationManager.AppSettings["email"];
+            _nationality = ConfigurationManager.AppSettings["nationality"];
+            _details = ConfigurationManager.AppSettings["details"];
+
+            Regex r2 = new Regex(@"(?<one>\d+)-(?<two>\d+)-(?<three>\d+)", RegexOptions.IgnoreCase | RegexOptions.Singleline);
+
+            Match m2 = r2.Match(ConfigurationManager.AppSettings["phone"].ToString());
+
+            while (m2.Success)
+            {
+                _uphone[0] = m2.Result("${one}");
+                _uphone[1] = m2.Result("${two}");
+                _uphone[2] = m2.Result("${three}");
+                m2 = m2.NextMatch();
+            }
+
+
+        }
+
+        private void buttonまずここ_Click(object sender, EventArgs e)
+        {
+            HtmlDocument doc = setLoadAttri();
+        }
+
+        public string yuubin(string inputStr)
+        {
+
+            Regex r = new Regex(@"〒(?<ue>\d+)-(?<sita>\d+)", RegexOptions.IgnoreCase | RegexOptions.Singleline);
+
+            Match m = r.Match(inputStr);
+
+            while (m.Success)
+            {
+                //Console.WriteLine(m.Result("${ue}${sita}"));
+                return m.Result("${ue}${sita}");
+                m = m.NextMatch();
+            }
+
+            return null;
+        }
+
+        public string kenmei(string number)
+        {
+            if (number != null)
+            {
+                //DataRow[] dataRows = dataset.Tables["Table"].Select("yubin = '" + number + "'");
+
+                DataRow[] dataRows = this._connectionKEN.Select("num = '" + number + "'");
+
+                foreach (DataRow row in dataRows)
+                {
+                    //Console.WriteLine(row[1].ToString());
+                    return row[1].ToString();
+                }
+            }
+
+            return "---選択してください---";
+        }
+
+        private HtmlDocument setWorkKen(string kenname)
+        {
+            HtmlDocument doc = this.webBrowser1.Document;
+
+            HtmlElementCollection selects = doc.GetElementsByTagName("select");
+            HtmlElement select = selects["workpref"];
+
+            if (select != null)
+            {
+                foreach (HtmlElement child in select.Children)
+                {
+                    if (child.InnerText == kenname)
+                    {
+                        child.SetAttribute("selected", "true");
+                    }
+                }
+            }
+            return doc;
+        }
+
+        private HtmlDocument setSex()
+        {
+            HtmlDocument doc = this.webBrowser1.Document;
+
+            HtmlElementCollection selects = doc.GetElementsByTagName("select");
+            HtmlElement select = selects["sextype"];
+
+            if (select != null)
+            {
+                foreach (HtmlElement child in select.Children)
+                {
+                    if (child.InnerText == _usex)
+                    {
+                        child.SetAttribute("selected", "true");
+                    }
+                }
+            }
+            return doc;
+        }
+
+        private HtmlDocument setken()
+        {
+            HtmlDocument doc = this.webBrowser1.Document;
+
+            HtmlElementCollection selects = doc.GetElementsByTagName("select");
+            HtmlElement select = selects["usrpref"];
+
+            if (select != null)
+            {
+                foreach (HtmlElement child in select.Children)
+                {
+                    if (child.InnerText == _ucontry)
+                    {
+                        child.SetAttribute("selected", "true");
+                    }
+                }
+            }
+            return doc;
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            string htmltxt = this.webBrowser1.Document.Body.InnerText;
+
+            Regex r2 = new Regex(@"送信完了", RegexOptions.IgnoreCase | RegexOptions.Singleline);
+
+            Match m2 = r2.Match(htmltxt);
+
+            if (m2.Success)
+            {
+
+                Regex r = new Regex(@"(?<num>\d+)", RegexOptions.IgnoreCase | RegexOptions.Singleline);
+
+                Match m = r.Match(htmltxt);
+
+                while (m.Success)
+                {
+                    if (dataGridView1.CurrentRow != null)
+                    {
+                        this.dataGridView1.CurrentRow.Cells[4].Value = m.Result("${num}");
+                        this.dataGridView1.CurrentRow.Cells[5].Value = "済";
+                        this.dataGridView1.EndEdit();
+
+                        csvSave();
+                    }
+                    m = m.NextMatch();
+
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            //DataTable dtable = (DataTable)dataGridView1.DataSource;
+            //DataRow darow = dtable.Rows[dataGridView1.CurrentRow.Index];
+
+            string htmltxt = this.webBrowser1.Document.Body.InnerText;
+
+            Regex r2 = new Regex(@"送信完了", RegexOptions.IgnoreCase | RegexOptions.Singleline);
+
+            Match m2 = r2.Match(htmltxt);
+
+            if (m2.Success)
+            {
+
+                Regex r = new Regex(@"(?<num>\d+)", RegexOptions.IgnoreCase | RegexOptions.Singleline);
+
+                Match m = r.Match(htmltxt);
+
+                while (m.Success)
+                {
+                    if (dataGridView1.CurrentRow != null)
+                    {
+                        this.dataGridView1.CurrentRow.Cells[4].Value = m.Result("${num}");
+                        this.dataGridView1.CurrentRow.Cells[5].Value = "済";
+                        this.dataGridView1.EndEdit();
+
+                        //dtable.Rows[dataGridView1.CurrentRow.Index][4] = m.Result("${num}");
+                        //dtable.Rows[dataGridView1.CurrentRow.Index][5] = "済";
+                        //drow.Cells[4].Value = m.Result("${num}");
+                        //drow.Cells[5].Value = "済";
+                        //dtable.Rows[dataGridView1.CurrentRow.Index].EndEdit();
+                        //drow.endedit();
+                        csvSave();
+                    }
+                    m = m.NextMatch();
+
+                }
+            }
+
+        }
+
+        public void csvSave()
+        {
+            int rows = this.dataGridView1.RowCount - 1;
+            int cols = this.dataGridView1.ColumnCount;
+            int lastColIndex = cols - 1;
+
+            StreamWriter sw = new StreamWriter(configFilePath, false, System.Text.Encoding.GetEncoding("SHIFT-JIS"));
+
+            for (int t = 0; t < rows; t++)
+            {
+                for (int i = 0; i < cols; i++)
+                {
+                    if (this.dataGridView1.Rows[t].Cells[i].Value != null)
+                    {
+                        string field = this.dataGridView1.Rows[t].Cells[i].Value.ToString();
+                        sw.Write(field);
+                    }
+                    if (lastColIndex > i)
+                    {
+                        sw.Write(',');
+                    }
+                }
+                sw.Write("\r\n");
+            }
+
+            sw.Close();
+
+        }
+
+        public string kenmeiGet(string address)
+        {
+            Regex r = new Regex(@"(?<ken>.{2}[都道府県]|.{3}県)", RegexOptions.IgnoreCase | RegexOptions.Singleline);
+
+            Match m = r.Match(address);
+
+            while (m.Success)
+            {
+                //Console.WriteLine(m.Result("${ue}${sita}"));
+                return m.Result("${ken}");
+                m = m.NextMatch();
+            }
+
+            return "---選択してください---";
+        }
+
+    }
+}
